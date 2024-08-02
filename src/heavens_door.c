c06976fb2f760fcdd0dc2691e71fc1fee02c25f3
Turtel216 <papadimitrios216@gmail.com>
2024-08-03 01:08:41 +0300

    Updated input handling

--- src/heavens_door.c
+++ src/heavens_door.c
@@ -1,14 +1,27 @@
 #include "heavens_door.h"
+#include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
+struct termios _termios;
+
+static void disable_RawMode(void)
+{
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &_termios);
+}
+
 void enable_RowMode(void)
 {
-	struct termios raw;
+	tcgetattr(STDIN_FILENO, &_termios);
+	atexit(disable_RawMode);
 
-	tcgetattr(STDIN_FILENO, &raw);
-
-	raw.c_lflag &= ~(ECHO);
+	struct termios raw = _termios;
+	raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+	raw.c_oflag &= ~(OPOST);
+	raw.c_cflag |= (CS8);
+	raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+	raw.c_cc[VMIN] = 0;
+	raw.c_cc[VTIME] = 1;
 
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
--- src/main.c
+++ src/main.c
@@ -1,3 +1,5 @@
+#include <ctype.h>
+#include <stdio.h>
 #include <unistd.h>
 #include "exit_codes.h"
 #include "heavens_door.h"
@@ -6,8 +8,19 @@ int main(void)
 {
 	enable_RowMode();
 
-	char c;
-	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
-		;
+	while (1) {
+		char c = '\0';
+		read(STDIN_FILENO, &c, 1);
+
+		if (iscntrl(c)) {
+			printf("%d\r\n", c);
+		} else {
+			printf("%d ('%c')\r\n", c, c);
+		}
+
+		if (c == 'q')
+			break;
+	}
+
 	return EXIT_SUCCESS;
 }
